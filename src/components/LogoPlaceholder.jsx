309f8b4fbb77c1d8dc4ed6c52502d305cc85f88f
AlvitoBryan <alvtbryn1@gmail.com>
2025-03-17 14:48:44 +0700

    🎨 setup folder structure, install tailwind and install react-router-dom

---
+++ src/components/LogoPlaceholder.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const LogoPlaceholder = () => {
+    return (
+        <div>
+            <img src="/src/assets/Logo.svg"></img>
+        </div>
+    )
+}
+export default LogoPlaceholder;
---
+++ src/pages/beranda/Homepage.jsx
@@ -0,0 +1,11 @@
+import LogoPlaceholder from "../../components/LogoPlaceholder";
+
+const Homepage = () => {
+    return(
+        <>
+            <LogoPlaceholder/>
+        </>
+    )
+}
+
+export default Homepage;
