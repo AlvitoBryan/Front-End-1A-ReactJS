9f9809a106b5c250efdfea43b9be39cebcb153eb
AlvitoBryan <alvtbryn1@gmail.com>
2025-03-24 15:19:34 +0700

    🐛 deploy fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "/src/pages/beranda/Homepage.jsx";
-import Login from "/src/pages/login/Login.jsx";
+import Login from "/src/pages/login/login.jsx";
 import Register from "/src/pages/register/Register.jsx";
 
 import '/src/globals.css'
